983e9ef26d2b5843a96e279122861d659d72738e
Camsca <camila_vass@hotmail.com>
2023-12-21 22:33:21 -0800

    events for pwa installation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+ event.preventDefault();
+ installPrompt = event;
+butInstall.removeAttribute("hidden");  
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+});
+//click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    if (installPrompt) {
+      try {
+        const choiceResult = await installPrompt.prompt();
+  
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the installation');
+        } else {
+          console.log('User dismissed the installation');
+        }
+      } catch (error) {
+        console.error('Error occurred during installation:', error);
+      } finally {
+        installPrompt = null; 
+        butInstall.setAttribute('hidden', ''); 
+      }
+    }
+  });
+
+
+window.addEventListener('appinstalled', (event) => {
+console.log('👍', 'appinstalled', event);
+});
